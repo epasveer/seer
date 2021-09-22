2e00de53f02c2962858f31611cfca16cdefc95c9
Ernie Pasveer <epasveer@att.net>
2021-09-22 11:55:58 -0500

    Increment version number.

--- src/seer.cpp
+++ src/seer.cpp
@@ -12,7 +12,7 @@ int main (int argc, char* argv[]) {
     QCoreApplication::setApplicationName("Seer");
     QCoreApplication::setOrganizationName("Pasvix");
     QCoreApplication::setOrganizationName("pasvix.com");
-    QCoreApplication::setApplicationVersion("1.0.3 - Ernie Pasveer (c)2021");
+    QCoreApplication::setApplicationVersion("1.0.4 - Ernie Pasveer (c)2021");
 
     //
     // Parse arguments.
