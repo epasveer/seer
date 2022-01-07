201b085b76c5348f70ad730e3b5436a307b1d14d
Ernie Pasveer <epasveer@att.net>
2022-01-07 17:33:16 -0600

    Checkpoint.

--- src/SeerHighlighterSettings.cpp
+++ src/SeerHighlighterSettings.cpp
@@ -48,25 +48,57 @@ void SeerHighlighterSettings::add (const QString& name, QTextCharFormat& format)
 SeerHighlighterSettings SeerHighlighterSettings::populateForCPP () {
 
     SeerHighlighterSettings cppSettings;
+
+
     QTextCharFormat f;
+
+    f.setFontWeight(QFont::Normal); f.setFontItalic(false); f.setForeground(Qt::white);
+    cppSettings.add("Background", f);
+    f = QTextCharFormat();
+
+    f.setFontWeight(QFont::Normal); f.setFontItalic(false); f.setForeground(Qt::black);
+    cppSettings.add("Foreground", f);
+    f = QTextCharFormat();
+
+    f.setFontWeight(QFont::Normal); f.setFontItalic(false); f.setForeground(Qt::lightGray);
+    cppSettings.add("Margin Background", f);
+    f = QTextCharFormat();
+
+    f.setFontWeight(QFont::Normal); f.setFontItalic(false); f.setForeground(Qt::black);
+    cppSettings.add("Margin Foreground", f);
+    f = QTextCharFormat();
 
     f.setFontWeight(QFont::Bold); f.setFontItalic(false); f.setForeground(Qt::darkMagenta);
     cppSettings.add("Class", f);
+    f = QTextCharFormat();
 
     f.setFontWeight(QFont::Normal); f.setFontItalic(false); f.setForeground(Qt::darkGreen);
     cppSettings.add("Quotation", f);
+    f = QTextCharFormat();
 
     f.setFontWeight(QFont::Normal); f.setFontItalic(true); f.setForeground(Qt::blue);
     cppSettings.add("Function", f);
+    f = QTextCharFormat();
 
     f.setFontWeight(QFont::Normal); f.setFontItalic(false); f.setForeground(Qt::red);
     cppSettings.add("Comment", f);
+    f = QTextCharFormat();
 
     f.setFontWeight(QFont::Normal); f.setFontItalic(false); f.setForeground(Qt::red);
     cppSettings.add("Multiline Comment", f);
+    f = QTextCharFormat();
 
     f.setFontWeight(QFont::Bold); f.setFontItalic(false); f.setForeground(Qt::darkBlue);
     cppSettings.add("Keyword", f);
+    f = QTextCharFormat();
+
+    f.setFontWeight(QFont::Normal); f.setFontItalic(false); f.setForeground(QColor(Qt::yellow).lighter(160));
+    cppSettings.add("Current Line", f);
+    f = QTextCharFormat();
+
+    f.setFontWeight(QFont::Normal); f.setFontItalic(false); f.setForeground(QColor(Qt::lightGray).lighter(120));
+    cppSettings.add("Current Line2", f);
+    f = QTextCharFormat();
 
     return cppSettings;
 }
