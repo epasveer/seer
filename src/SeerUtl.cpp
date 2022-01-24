42b3588803f1fa1b083f002e96d5382377a1c83a
Ernie Pasveer <epasveer@att.net>
2022-01-24 13:39:00 -0600

    Increment version number for next release.

--- src/SeerUtl.cpp
+++ src/SeerUtl.cpp
@@ -6,7 +6,7 @@ namespace Seer {
     QString version () {
 
         // Increment this with every release on GitHub.
-        return "1.0.7";
+        return "1.0.8";
     }
 
     QString filterEscapes (const QString& str) {
