8fd9c78ed7650d53789fde81871677c6c76ca037
Ernie Pasveer <epasveer@att.net>
2022-02-20 11:02:27 -0600

    Incrementing version number for next release.
    Switching to v1.x format instead of v1.0.x

--- src/SeerUtl.cpp
+++ src/SeerUtl.cpp
@@ -6,7 +6,7 @@ namespace Seer {
     QString version () {
 
         // Increment this with every release on GitHub.
-        return "1.0.9";
+        return "1.1";
     }
 
     QString filterEscapes (const QString& str) {
