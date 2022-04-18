fbf00198c368b28d748d4eb8d4c02511bce9ce87
Ernie Pasveer <epasveer@att.net>
2022-04-18 13:39:49 -0500

    Add another way to run the makefile using cmake itself.

--- src/build/README.cmake
+++ src/build/README.cmake
@@ -4,9 +4,13 @@ Notes for building Seer with cmake.
 % cmake -DCMAKE_BUILD_TYPE=Debug ..     # Debug release -g.
 % cmake -DCMAKE_BUILD_TYPE=Release ..   # Optimized release -O.
 % cmake -DCMAKE_CXX_FLAGS=-Wall ..      # With all compile warnings turned on.
+
+
 % make clean                            # Clean files.
-
 % make seer                             # Build Seer.
-
 % sudo make install                     # Install it.
+
+% cmake --build . --config Release
+% sudo cmake --install .
+
 
