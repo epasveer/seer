e24d033f63bdebf4d8906985ae466953b5b801c3
Ernie Pasveer <epasveer@att.net>
2022-10-02 12:50:07 -0500

    Support older Qt5.12 version.

--- src/SeerMainWindow.cpp
+++ src/SeerMainWindow.cpp
@@ -47,7 +47,11 @@ SeerMainWindow::SeerMainWindow(QWidget* parent) : QMainWindow(parent) {
     // Set up Styles menu.
     //
     _styleMenuActionGroup = new QActionGroup(this);
+#if (QT_VERSION >= QT_VERSION_CHECK(5, 14, 0))
     _styleMenuActionGroup->setExclusionPolicy(QActionGroup::ExclusionPolicy::Exclusive);
+#else
+    _styleMenuActionGroup->setExclusive(true);
+#endif
     _styleMenuActionGroup->setEnabled(true);
     _styleMenuActionGroup->setVisible(true);
 
