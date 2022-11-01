3c7b4b87b25e9cdfaded24e3d8821a6ff652d8c2
Ernie Pasveer <epasveer@att.net>
2022-11-01 11:33:03 -0500

    Tweaks notest.

--- tests/helloworld/helloworld.cpp
+++ tests/helloworld/helloworld.cpp
@@ -10,7 +10,11 @@ int main (int argc, char** argv) {
     int j = 0;
 
     for (int i=0; i<argc; i++) {
-        std::cout << "XX: " << i << " " << argv[i] << std::endl;
+        std::cout << "XX: "
+                  << i
+                  << " "
+                  << argv[i]
+                  << std::endl;
     }
 
     std::string message = "Hello, World!";
