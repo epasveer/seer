ad8f95d6b9ec2a8ba4a27a030e485462bc6b738d
Ernie Pasveer <epasveer@att.net>
2023-03-05 09:38:39 -0600

    Swapped Lena image with another PD image.
    Lena needs a break from the Internet. :^)

--- tests/helloimage/helloimage.cpp
+++ tests/helloimage/helloimage.cpp
@@ -9,7 +9,7 @@ int main (int argc, char** argv) {
 
     std::cout << "Hello, Image!" << std::endl;
 
-    QImageReader reader("lena.bmp");
+    QImageReader reader("mona.jpg");
     reader.setAutoTransform(true);
 
     QImage image = reader.read();
@@ -20,13 +20,13 @@ int main (int argc, char** argv) {
     QImage image_rgb  = image.convertToFormat(QImage::Format_RGB888);
     QImage image_rgba = image.convertToFormat(QImage::Format_RGBA8888);
 
-    qDebug() << image;
-    qDebug() << image_rgb;
-    qDebug() << image_rgba;
+    qInfo() << image;
+    qInfo() << image_rgb;
+    qInfo() << image_rgba;
 
-    // QImage(QSize(512, 512),format=QImage::Format_Indexed8,depth=8,colorCount=256,devicePixelRatio=1,bytesPerLine=512,sizeInBytes=262144)
-    // QImage(QSize(512, 512),format=QImage::Format_RGB888,depth=24,devicePixelRatio=1,bytesPerLine=1536,sizeInBytes=786432)
-    // QImage(QSize(512, 512),format=QImage::Format_RGBA8888,depth=32,devicePixelRatio=1,bytesPerLine=2048,sizeInBytes=1048576)
+    // QImage(QSize(220, 197),format=QImage::Format_RGB32,depth=32,devicePixelRatio=1,bytesPerLine=880,sizeInBytes=173360)
+    // QImage(QSize(220, 197),format=QImage::Format_RGB888,depth=24,devicePixelRatio=1,bytesPerLine=660,sizeInBytes=130020)
+    // QImage(QSize(220, 197),format=QImage::Format_RGBA8888,depth=32,devicePixelRatio=1,bytesPerLine=880,sizeInBytes=173360)
 
     const uchar* bits      = image.bits();
     const uchar* bits_rgb  = image_rgb.bits();
