8864cb7a4011e171978f6cc6a31269993926873d
Ernie Pasveer <epasveer@att.net>
2023-04-29 09:00:29 -0500

    Change Rust program to use threads.

--- tests/hellorust/hellorust.rs
+++ tests/hellorust/hellorust.rs
@@ -1,37 +1,24 @@
-// Unlike C/C++, there's no restriction on the order of function definitions
+
+use std::thread;
+use std::time::Duration;
+
 fn main() {
-    // We can use this function here, and define it somewhere later
-    fizzbuzz_to(100);
-}
 
-// Function that returns a boolean value
-fn is_divisible_by(lhs: u32, rhs: u32) -> bool {
-    // Corner case, early return
-    if rhs == 0 {
-        return false;
-    }
+    println!("Hello, Rust world!");
 
-    // This is an expression, the `return` keyword is not necessary here
-    lhs % rhs == 0
-}
+    let handle = thread::spawn(|| {
+        for i in 1..10 {
+            println!("hi number {} from the spawned thread!", i);
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
 
-// Functions that "don't" return a value, actually return the unit type `()`
-fn fizzbuzz(n: u32) -> () {
-    if is_divisible_by(n, 15) {
-        println!("fizzbuzz");
-    } else if is_divisible_by(n, 3) {
-        println!("fizz");
-    } else if is_divisible_by(n, 5) {
-        println!("buzz");
-    } else {
-        println!("{}", n);
+    handle.join().unwrap();
+
+    for i in 1..5 {
+        println!("hi number {} from the main thread!", i);
+        thread::sleep(Duration::from_millis(1));
     }
 }
 
-// When a function returns `()`, the return type can be omitted from the
-// signature
-fn fizzbuzz_to(n: u32) {
-    for n in 1..=n {
-        fizzbuzz(n);
-    }
-}
+
