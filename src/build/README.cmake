220588cfa1359f2a01149dd51927a07014d11e52
Ernie Pasveer <epasveer@att.net>
2023-04-30 16:02:45 -0500

    Added more info for building on MacOS.

--- src/build/README.cmake
+++ src/build/README.cmake
@@ -1,16 +1,19 @@
 Notes for building Seer with cmake.
 
-% cd seer/src/build
-% cmake -DCMAKE_BUILD_TYPE=Debug ..     # Debug release -g.
-% cmake -DCMAKE_BUILD_TYPE=Release ..   # Optimized release -O.
-% cmake -DCMAKE_CXX_FLAGS=-Wall ..      # With all compile warnings turned on.
+    % cd seer/src/build
+    % cmake -DCMAKE_BUILD_TYPE=Debug ..     # Debug release -g.
+    % cmake -DCMAKE_BUILD_TYPE=Release ..   # Optimized release -O.
+    % cmake -DCMAKE_CXX_FLAGS=-Wall ..      # With all compile warnings turned on.
 
 
-% make clean                            # Clean files.
-% make seergdb                          # Build Seer.
-% sudo make install                     # Install it.
+    % make clean                            # Clean files.
+    % make seergdb                          # Build Seer.
+    % sudo make install                     # Install it.
 
-% cmake --build . --config Release
-% sudo cmake --install .
+    % cmake --build . --config Release
+    % sudo cmake --install .
 
+MacOS may need help finding the cmake config file for Qt5.
+
+    % cmake -DCMAKE_PREFIX_PATH=/usr/local/opt/qt5/ -DCMAKE_BUILD_TYPE=Release ..
 
