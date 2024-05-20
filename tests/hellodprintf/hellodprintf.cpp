07d677cf1e439085a86eb83cd7b63bf5592e7b88
Ernie Pasveer <epasveer@att.net>
2024-05-20 15:50:54 -0500

    Checkpoint with dprintf.

--- tests/hellodprintf/hellodprintf.cpp
+++ tests/hellodprintf/hellodprintf.cpp
@@ -1,12 +1,39 @@
 #include <string>
 #include <iostream>
 #include <iomanip>
+#include <stdio.h>
+#include <stdarg.h>
+
+FILE* logfile = NULL;
 
 extern "C" {
-void fortranfunction_ (int* count);
+    int log_msgxxx (const char* format, ...) {
+        int n;
+        va_list args;
+        // init the variable length argument list
+        va_start(args, format);
+
+        n = vfprintf(logfile, format, args);
+
+        // cleanup the variable length argument list
+        va_end(args);
+
+        return n;
+    }
+}
+
+extern "C" {
+    void fortranfunction_ (int* count);
 }
 
 int main (int argc, char** argv) {
+
+    const char* filename = "logfile.txt";
+    logfile = fopen(filename, "w");
+    if (logfile == NULL) {
+        fprintf(stderr, "Failed to open logfile: %s\n", filename);
+        return 1;
+    }
 
     int count = 10;
     int nfact = 1;
@@ -19,7 +46,9 @@ int main (int argc, char** argv) {
         std::cout << std::setw(12)  << n << std::setw(14) << nfact << std::endl;
     }
 
-    fortranfunction_(&count);
+    // fortranfunction_(&count);
+
+    fclose(logfile);
 
     return 0;
 }
