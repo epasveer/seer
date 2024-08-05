2b0bc3108c3952e01d41776b8e8975e374484f9f
Ernie Pasveer <epasveer@att.net>
2024-08-05 17:40:49 -0500

    Updated ansicolor2 test program.

--- tests/helloansicodes2/printfcolor.h
+++ tests/helloansicodes2/printfcolor.h
@@ -2,6 +2,7 @@
 #define PRINTFCOLOR_H
 
 #include <stdio.h>
+#include <errno.h>
 #include <stdarg.h>
 #include <stdbool.h>
 
@@ -14,7 +15,7 @@ extern "C" {
 #endif
 
 
-typedef enum Color {
+typedef enum PfcColor {
     GRAY = 0,
     GREY = 0,
     BLUE = 1,
@@ -24,237 +25,232 @@ typedef enum Color {
     MAGENTA = 5,
     YELLOW = 6,
     WHITE = 7
-} Color;
+} PfcColor;
+
 
 /**
  * Prints formatted output to the console with specified foreground and background colors.
  *
- * @param foregroundColor  The color code for the text foreground color. 
- * @param backgroundColor  The color code for the text background color. 
- * @param format           A format string for the output. This is followed by additional arguments
- *                         to be formatted according to the format string.
+ * Colors: `RED, GREEN, BLUE, WHITE, GRAY (GREY), CYAN, MAGENTA, YELLOW`
+ * 
+ * @param fg_color  The color code for the text foreground color. 
+ * @param bg_color  The color code for the text background color. 
+ * @param format    A format string for the output. This is followed by additional arguments
+ *                  to be formatted according to the format string.
  *
- * @return  The number of characters written to the console, or `-1` if an error occurs. 
- *          Returns `-1` if the provided color codes are invalid or if there is an issue retrieving
- *          the console screen buffer information.
+ * @return  The number of characters written to the console, or `-1` if an error occurs.
  *
  * @warning If the color codes are invalid or if the console screen buffer information cannot
- *          be retrieved, an error message is printed to `stderr`, and `-1` is returned.
+ *          be retrieved, `errno` is set to `EINVAL` and `-1` is returned. 
  */
-int printfColor(Color foregroundColor, Color backgroundColor, const char *format, ...);
+int printfc(PfcColor fg_color, PfcColor bg_color, const char *format, ...);
 
 /**
  * Prints formatted output to the console with specified foreground color.
+ * 
+ * Colors: `RED, GREEN, BLUE, WHITE, GRAY (GREY), CYAN, MAGENTA, YELLOW`
  *
- * @param foregroundColor  The color code for the text foreground color.
- * @param format           A format string for the output. This is followed by additional arguments
- *                         to be formatted according to the format string.
+ * @param fg_color  The color code for the text foreground color.
+ * @param format    A format string for the output. This is followed by additional arguments
+ *                  to be formatted according to the format string.
  *
- * @return  The number of characters written to the console, or `-1` if an error occurs. 
- *          Returns `-1` if the provided color codes are invalid or if there is an issue retrieving
- *          the console screen buffer information.
+ * @return  The number of characters written to the console, or `-1` if an error occurs.
  *
  * @warning If the color code is invalid or if the console screen buffer information cannot
- *          be retrieved, an error message is printed to `stderr`, and `-1` is returned.
+ *          be retrieved, `errno` is set to `EINVAL` and `-1` is returned. 
  */
-int printfColorFg(Color foregroundColor, const char *format, ...);
+int printfc_fg(PfcColor fg_color, const char *format, ...);
 
 /**
  * Prints formatted output to the console with specified background color.
+ * 
+ * Colors: `RED, GREEN, BLUE, WHITE, GRAY (GREY), CYAN, MAGENTA, YELLOW`
  *
- * @param backgroundColor  The color code for the text background color.
- * @param format           A format string for the output. This is followed by additional arguments
- *                         to be formatted according to the format string.
+ * @param bg_color  The color code for the text background color.
+ * @param format    A format string for the output. This is followed by additional arguments
+ *                  to be formatted according to the format string.
  *
- * @return  The number of characters written to the console, or `-1` if an error occurs. 
- *          Returns `-1` if the provided color codes are invalid or if there is an issue retrieving
- *          the console screen buffer information.
+ * @return  The number of characters written to the console, or `-1` if an error occurs.
  *
  * @warning If the color code is invalid or if the console screen buffer information cannot
- *          be retrieved, an error message is printed to `stderr`, and `-1` is returned.
+ *          be retrieved, `errno` is set to `EINVAL` and `-1` is returned. 
  */
-int printfColorBg(Color backgroundColor, const char *format, ...);
+int printfc_bg(PfcColor bg_color, const char *format, ...);
 
 
 // Helper functions
 
-const char* getForegroundColorCode(Color color) {
-    switch (color) {
-        case GRAY:    return "\033[30m";
-        case BLUE:    return "\033[34m";
-        case GREEN:   return "\033[32m";
-        case CYAN:    return "\033[36m";
-        case RED:     return "\033[31m";
-        case MAGENTA: return "\033[35m";
-        case YELLOW:  return "\033[33m";
-        case WHITE:   return "\033[37m";
-        default:      return NULL; // Return NULL for invalid colors
-    }
-}
-
-const char* getBackgroundColorCode(Color color) {
-    switch (color) {
-        case GRAY:    return "\033[40m";
-        case BLUE:    return "\033[44m";
-        case GREEN:   return "\033[42m";
-        case CYAN:    return "\033[46m";
-        case RED:     return "\033[41m";
-        case MAGENTA: return "\033[45m";
-        case YELLOW:  return "\033[43m";
-        case WHITE:   return "\033[47m";
-        default:      return NULL; // Return NULL for invalid colors
-    }
-}
-
-bool isColorCodeValid(Color color)
+static bool is_color_valid(PfcColor color)
 {
     return color >= 0 && color <= 7;
 }
 
+static const char *get_fg_color_code(PfcColor color) {
+    const char *colors[] = { 
+        "\033[30m", "\033[34m", "\033[32m", "\033[36m",
+        "\033[31m", "\033[35m", "\033[33m", "\033[37m" 
+    };
+
+    if (is_color_valid(color))
+        return colors[color];
+    else
+        return NULL;
+}
+
+static const char *get_bg_color_code(PfcColor color) {
+    const char *colors[] = { 
+        "\033[40m", "\033[44m", "\033[42m", "\033[46m", 
+        "\033[41m", "\033[45m", "\033[43m", "\033[47m" 
+    };
+
+    if (is_color_valid(color))
+        return colors[color];
+    else
+        return NULL;
+}
+
 #ifdef _WIN32 // Windows
 
-int printfColor(Color foregroundColor, Color backgroundColor, const char *format, ...) 
+int printfc(PfcColor fg_color, PfcColor bg_color, const char *format, ...) 
 {
-    if (!isColorCodeValid(foregroundColor) || !isColorCodeValid(backgroundColor)) {
-        fprintf(stderr, "Error: Invalid color\n");
+    if (!is_color_valid(fg_color) || !is_color_valid(bg_color)) {
+        errno = EINVAL;
         return -1;
     }
 
-    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-    CONSOLE_SCREEN_BUFFER_INFO csbi;
+    HANDLE h_console = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_SCREEN_BUFFER_INFO csbi_info;
 
-    if (!GetConsoleScreenBufferInfo(hConsole, &csbi)) {
-        fprintf(stderr, "Error: Could not retrieve console screen buffer info\n");
+    if (!GetConsoleScreenBufferInfo(h_console, &csbi_info)) {
+        errno = EINVAL;
         return -1;
     }
 
     va_list args;
     va_start(args, format);
 
-    SetConsoleTextAttribute(hConsole, foregroundColor | (backgroundColor << 4));
-    int charsWritten = vprintf(format, args);
-    SetConsoleTextAttribute(hConsole, csbi.wAttributes);
+    SetConsoleTextAttribute(h_console, fg_color | (bg_color << 4));
+    int chars_written = vprintf(format, args);
+    SetConsoleTextAttribute(h_console, csbi_info.wAttributes);
 
     va_end(args);
 
-    return charsWritten;
+    return chars_written;
 }
 
-int printfColorFg(Color foregroundColor, const char *format, ...) 
+int printfc_fg(PfcColor fg_color, const char *format, ...) 
 {
-    if (!isColorCodeValid(foregroundColor)) {
-        fprintf(stderr, "Error: Invalid color\n");
+    if (!is_color_valid(fg_color)) {
+        errno = EINVAL;
         return -1;
     }
 
-    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-    CONSOLE_SCREEN_BUFFER_INFO csbi;
+    HANDLE h_console = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_SCREEN_BUFFER_INFO csbi_info;
 
-    if (!GetConsoleScreenBufferInfo(hConsole, &csbi)) {
-        fprintf(stderr, "Error: Could not retrieve console screen buffer info\n");
+    if (!GetConsoleScreenBufferInfo(h_console, &csbi_info)) {
+        errno = EINVAL;
+        return -1;
     }
 
     va_list args;
     va_start(args, format);
 
-    SetConsoleTextAttribute(hConsole, foregroundColor);
+    SetConsoleTextAttribute(h_console, fg_color);
 
-    int charsWritten = vprintf(format, args);
+    int chars_written = vprintf(format, args);
 
-    SetConsoleTextAttribute(hConsole, csbi.wAttributes);
+    SetConsoleTextAttribute(h_console, csbi_info.wAttributes);
 
     va_end(args);
 
-    return charsWritten;
+    return chars_written;
 }
 
-int printfColorBg(Color backgroundColor, const char *format, ...) 
+int printfc_bg(PfcColor bg_color, const char *format, ...) 
 {
-    if (!isColorCodeValid(backgroundColor)) {
-        fprintf(stderr, "Error: Invalid color\n");
+    if (!is_color_valid(bg_color)) {
+        errno = EINVAL;
         return -1;
     }
 
-    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-    CONSOLE_SCREEN_BUFFER_INFO csbi;
+    HANDLE h_console = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_SCREEN_BUFFER_INFO csbi_info;
 
-    if (!GetConsoleScreenBufferInfo(hConsole, &csbi)) {
-        fprintf(stderr, "Error: Could not retrieve console screen buffer info\n");
+    if (!GetConsoleScreenBufferInfo(h_console, &csbi_info)) {
+        errno = EINVAL;
         return -1;
     }
 
     va_list args;
     va_start(args, format);
 
-    SetConsoleTextAttribute(hConsole, (csbi.wAttributes & 0x0F) | (backgroundColor << 4));
-    int charsWritten = vprintf(format, args);
-    SetConsoleTextAttribute(hConsole, csbi.wAttributes);
+    SetConsoleTextAttribute(h_console, (csbi_info.wAttributes & 0x0F) | (bg_color << 4));
+    int chars_written = vprintf(format, args);
+    SetConsoleTextAttribute(h_console, csbi_info.wAttributes);
 
     va_end(args);
 
-    return charsWritten;
+    return chars_written;
 }
 
 #else // Linux and Unix-like
 
-int printfColor(Color foregroundColor, Color backgroundColor, const char *format, ...)
+int printfc(PfcColor fg_color, PfcColor bg_color, const char *format, ...)
 {
-    if (!isColorCodeValid(foregroundColor) || !isColorCodeValid(backgroundColor)) {
-        fprintf(stderr, "Error: Invalid color\n");
+    if (!is_color_valid(fg_color) || !is_color_valid(bg_color)) {
+        errno = EINVAL;
         return -1;
     }
-
-    printf("%s%s", getForegroundColorCode(foregroundColor), getBackgroundColorCode(backgroundColor));
 
     va_list args;
     va_start(args, format);
 
-    int charsWritten = vprintf(format, args);
+    printf("%s%s", get_fg_color_code(fg_color), get_bg_color_code(bg_color));
+    int chars_written = vprintf(format, args);
     printf("\033[0m");
 
     va_end(args);
 
-    return charsWritten;
+    return chars_written;
 }
 
-int printfColorFg(Color foregroundColor, const char *format, ...) 
+int printfc_fg(PfcColor fg_color, const char *format, ...) 
 {
-    if (!isColorCodeValid(foregroundColor)) {
-        fprintf(stderr, "Error: Invalid color\n");
+    if (!is_color_valid(fg_color)) {
+        errno = EINVAL;
         return -1;
     }
-
-    printf("%s", getForegroundColorCode(foregroundColor));
 
     va_list args;
     va_start(args, format);
 
-    int charsWritten = vprintf(format, args);
+    printf("%s", get_fg_color_code(fg_color));
+    int chars_written = vprintf(format, args);
     printf("\033[0m");
 
     va_end(args);
 
-    return charsWritten;
+    return chars_written;
 }
 
-int printfColorBg(Color backgroundColor, const char *format, ...) 
+int printfc_bg(PfcColor bg_color, const char *format, ...) 
 {
-    if (!isColorCodeValid(backgroundColor)) {
-        fprintf(stderr, "Error: Invalid color\n");
+    if (!is_color_valid(bg_color)) {
+        errno = EINVAL;
         return -1;
     }
-
-    printf("%s", getBackgroundColorCode(backgroundColor));
 
     va_list args;
     va_start(args, format);
 
-    int charsWritten = vprintf(format, args);
+    printf("%s", get_bg_color_code(bg_color));
+    int chars_written = vprintf(format, args);
     printf("\033[0m");
 
     va_end(args);
 
-    return charsWritten;
+    return chars_written;
 }
 
 #endif // _WIN32
