87088828a4af6cc6979ee837ea2779f9a5a0b742
RaphGL <raphfl.dev@gmail.com>
2025-09-29 18:18:58 -0100

    fixed odin syntax highlighting

--- src/SeerOdinSourceHighlighter.cpp
+++ src/SeerOdinSourceHighlighter.cpp
@@ -16,7 +16,6 @@ const SeerHighlighterSettings& SeerOdinSourceHighlighter::highlighterSettings() 
 }
 
 void SeerOdinSourceHighlighter::setHighlighterSettings (const SeerHighlighterSettings& settings) {
-
     _highlighterSettings     = settings;
 
     _classFormat             = _highlighterSettings.get("Class");
@@ -27,46 +26,45 @@ void SeerOdinSourceHighlighter::setHighlighterSettings (const SeerHighlighterSet
     _keywordFormat           = _highlighterSettings.get("Keyword");
 
     const QString keywordPatterns[] = {
-        QStringLiteral("\\package\\b"),
-        QStringLiteral("\\import\\b"),
-        QStringLiteral("\\foreign\\b"),
-        QStringLiteral("\\proc\\b"),
-        QStringLiteral("\\return\\b"),
-        QStringLiteral("\\if\\b"),
-        QStringLiteral("\\else\\b"),
-        QStringLiteral("\\switch\\b"),
-        QStringLiteral("\\case\\b"),
-        QStringLiteral("\\break\\b"),
-        QStringLiteral("\\continue\\b"),
-        QStringLiteral("\\fallthrough\\b"),
-        QStringLiteral("\\for\\b"),
-        QStringLiteral("\\do\\b"),
-        QStringLiteral("\\in\\b"),
-        QStringLiteral("\\not_in\\b"),
-        QStringLiteral("\\defer\\b"),
-        QStringLiteral("\\dynamic\\b"),
-        QStringLiteral("\\const\\b"),
-        QStringLiteral("\\where\\b"),
-        QStringLiteral("\\using\\b"),
-        QStringLiteral("\\or_else\\b"),
-        QStringLiteral("\\or_break\\b"),
-        QStringLiteral("\\or_continue\\b"),
-        QStringLiteral("\\or_return\\b"),
-        QStringLiteral("\\asm\\b"),
-        QStringLiteral("\\distinct\\b"),
-        QStringLiteral("\\context\\b"),
-        QStringLiteral("\\nil\\b"),
-        QStringLiteral("\\true\\b"),
-        QStringLiteral("\\false\\b"),
-        QStringLiteral("\\struct\\b"),
-        QStringLiteral("\\enum\\b"),
-        QStringLiteral("\\union\\b"),
-        QStringLiteral("\\map\\b"),
-        QStringLiteral("\\bit_set\\b"),
-        QStringLiteral("\\bit_field\\b"),
-        QStringLiteral("\\cast\\b"),
-        QStringLiteral("\\transmute\\b"),
-        QStringLiteral("\\auto_cast\\b"),
+        QStringLiteral("\\bpackage\\b"), QStringLiteral("\\bimport\\b"), QStringLiteral("\\bforeign\\b"),
+        QStringLiteral("\\bproc\\b"), QStringLiteral("\\breturn\\b"), QStringLiteral("\\bif\\b"),
+        QStringLiteral("\\belse\\b"), QStringLiteral("\\bswitch\\b"), QStringLiteral("\\bcase\\b"),
+        QStringLiteral("\\bbreak\\b"), QStringLiteral("\\bcontinue\\b"), QStringLiteral("\\bfallthrough\\b"),
+        QStringLiteral("\\bfor\\b"), QStringLiteral("\\bdo\\b"), QStringLiteral("\\bin\\b"),
+        QStringLiteral("\\bnot_in\\b"), QStringLiteral("\\bdefer\\b"), QStringLiteral("\\bdynamic\\b"),
+        QStringLiteral("\\bconst\\b"), QStringLiteral("\\bwhere\\b"), QStringLiteral("\\busing\\b"),
+        QStringLiteral("\\bor_else\\b"), QStringLiteral("\\bor_break\\b"), QStringLiteral("\\bor_continue\\b"),
+        QStringLiteral("\\bor_return\\b"), QStringLiteral("\\basm\\b"), QStringLiteral("\\bdistinct\\b"),
+        QStringLiteral("\\bcontext\\b"), QStringLiteral("\\bnil\\b"), QStringLiteral("\\btrue\\b"),
+        QStringLiteral("\\bfalse\\b"), QStringLiteral("\\bstruct\\b"), QStringLiteral("\\benum\\b"),
+        QStringLiteral("\\bunion\\b"), QStringLiteral("\\bmap\\b"), QStringLiteral("\\bbit_set\\b"),
+        QStringLiteral("\\bbit_field\\b"), QStringLiteral("\\bcast\\b"), QStringLiteral("\\btransmute\\b"),
+        QStringLiteral("\\bauto_cast\\b"), QStringLiteral("\\bwhen\\b"),
+
+        // builtin types
+        QStringLiteral("\\b(i8|i16|i32|i64|i128|int)\\b"),
+        QStringLiteral("\\b(u8|u16|u32|u64|u128|uint|uintptr)\\b"),
+        QStringLiteral("\\b(f16|f32|f64|f128)\\b"),
+        QStringLiteral("\\bf16le|f32le|f64le|f128le\\b"),
+        QStringLiteral("\\b(f16be|f32be|f64be|f128be)\\b"),
+        QStringLiteral("\\b(complex32|complex64|complex128)\\b"),
+        QStringLiteral("\\b(quaternion64|quaternion128|quaternion256)\\b"),
+        QStringLiteral("\\b(bool|b8|b16|b32|b64)\\b"),
+        QStringLiteral("\\b(string|cstring|rune)\\b"),
+        QStringLiteral("\\b(rawptr)\\b"),
+        QStringLiteral("\\b(any|typeid)\\b"),
+        QStringLiteral("\\b(byte)\\b"),
+        QStringLiteral("\\b(u16le|u32le|u64le|u128le|i16le|i32le|i64le|i128le)\\b"),
+        QStringLiteral("\\b(i16be|i32be|i64be|i128be|u16be|u32be|u64be|u128be)\\b"),
+
+        // matches all the #+ and # compiler directives/builtins
+        QStringLiteral("#([^\\s\\(]+)(\\([^\\)]*\\))?"),
+        // matches all attributes like @(attribute) with optional parentheses
+        QStringLiteral("@(\\([^\\)]*\\)|\\w+)"),
+    };
+
+    const QString typePatterns[] = {
+      QStringLiteral("")
     };
 
     _highlightingRules.clear(); // Clear old rules.
@@ -74,7 +72,7 @@ void SeerOdinSourceHighlighter::setHighlighterSettings (const SeerHighlighterSet
     HighlightingRule rule;
 
     // Set class format and expression.
-    rule.pattern = QRegularExpression(QStringLiteral("\\bQ[A-Za-z]+\\b"));
+    rule.pattern = QRegularExpression(QStringLiteral("\\[A-Za-z]+\\b"));
     rule.format = _classFormat;
     _highlightingRules.append(rule);
 
