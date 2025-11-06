635188577582c59933d7c01287281327945ecc99
QuangNguyenMinh123 <100016403+QuangNguyenMinh123@users.noreply.github.com>
2025-11-06 11:43:11 +0700

    add recursive test, Rpi4 and stm32 build script

---
+++ tests/recursive_test/src/main.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include "arithmetic.h"
+#include "advanced.h"
+#include <chrono>
+#include <thread>
+#include <unistd.h>
+using namespace std;
+
+struct childStruct
+{
+    int childId;
+    string childString;
+};
+
+struct structTetst
+{
+    int id;
+    string b;
+    childStruct child;
+};
+struct structTetst globalStruct = {-2, "test", {-1, "childTest"}};
+/***
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+int main() {
+    globalStruct.id =0;
+    int a = 10, b = 3;
+    pid_t pid = getpid();
+    std::cout << "Process ID: " << pid << std::endl;
+    while (globalStruct.id < 100)
+    {
+        globalStruct.id++;
+        globalStruct.child.childId = globalStruct.id;
+        globalStruct.child.childString = "Child String " + std::to_string(globalStruct.id);
+        cout << "Basic Arithmetic:" << endl;
+        cout << a << " + " << b << " = " << add(a, b) << endl;
+        cout << a << " - " << b << " = " << subtract(a, b) << endl;
+        cout << a << " * " << b << " = " << multiply(a, b) << endl;
+        cout << a << " / " << b << " = " << divide(a, b) << endl;
+
+        cout << "\nAdvanced Math:" << endl;
+        cout << "Factorial of 5 = " << factorial(5) << endl;
+        cout << "Fibonacci of 7 = " << fibonacci(7) << endl;
+        globalStruct.child.childId ++;
+        std::this_thread::sleep_for(std::chrono::seconds(2));
+    }
+    return 0;
+}
