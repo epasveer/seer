59f61e7e95ef0e8ced9feab0671e39ee0efde7d2
QuangNguyenMinh123 <100016403+QuangNguyenMinh123@users.noreply.github.com>
2025-11-08 23:38:05 +0700

    update

--- src/SeerUtl.cpp
+++ src/SeerUtl.cpp
@@ -464,70 +464,70 @@ namespace Seer {
 
     QStringList parseCommaList  (const QString& str, QChar startBracket, QChar endBracket) {
 
-    // name = "Pasveer, Ernie", age = 60, salary = 0.25, location = {city = "Houston", state = "Texas", zip = 77063}
-    //
-    // name = "Pasveer, Ernie"
-    // age = 60
-    // salary = 0.25
-    // location = {city = "Houston", state = "Texas", zip = 77063}
-    //
-    // special case: garbage value:
-    // {id = 2, b = \"000hildTest 2000377177000000260324377377377177\", '000' <repeats 11 times>, \"hildTest 2000-", '0' <repeats 11 times>, "1", '0' <repeats 21 times>, "330324377377377177000000004", '0' <repeats 21 times>, "test\", '000' <repeats 21 times>, \"325377377\", child = {childId = -9864, childString = \"\"}}
+        // name = "Pasveer, Ernie", age = 60, salary = 0.25, location = {city = "Houston", state = "Texas", zip = 77063}
+        //
+        // name = "Pasveer, Ernie"
+        // age = 60
+        // salary = 0.25
+        // location = {city = "Houston", state = "Texas", zip = 77063}
+        //
+        // special case: garbage value:
+        // {id = 2, b = \"000hildTest 2000377177000000260324377377377177\", '000' <repeats 11 times>, \"hildTest 2000-", '0' <repeats 11 times>, "1", '0' <repeats 21 times>, "330324377377377177000000004", '0' <repeats 21 times>, "test\", '000' <repeats 21 times>, \"325377377\", child = {childId = -9864, childString = \"\"}}
 
-    enum STATE{
-        START       = 0,
-        SCANNING    = 1,
-        FOUND       = 2,
-        IGNORE      = 3,
-    };
-    QStringList list;
-    int         index        = 0;
-    int         start        = 0;
-    int         end          = 0;
-    int         previousComma= 0;
-    STATE       state        = START;
+        enum STATE{
+            START       = 0,
+            SCANNING    = 1,
+            FOUND       = 2,
+            IGNORE      = 3,
+        };
+        QStringList list;
+        int         index        = 0;
+        int         start        = 0;
+        int         end          = 0;
+        int         previousComma= 0;
+        STATE       state        = START;
 
-    while (index < str.length())
-    {
-        // Handle "="
-        if (str[index] == '=') {
-            if (state == SCANNING)               // looking for 2nd '='
-            {
-                // state = FOUND;
-                end = previousComma;
-                QString field = str.mid(start, end-start);
-                list.append(field.trimmed());
-                start = previousComma + 1;
+        while (index < str.length())
+        {
+            // Handle "="
+            if (str[index] == '=') {
+                if (state == SCANNING)               // looking for 2nd '='
+                {
+                    // state = FOUND;
+                    end = previousComma;
+                    QString field = str.mid(start, end-start);
+                    list.append(field.trimmed());
+                    start = previousComma + 1;
+                }
+                if (state == START)               // looking for 2nd '='
+                {
+                    state = SCANNING;
+
+                }
             }
-            if (state == START)               // looking for 2nd '='
+
+            if (str[index] == startBracket)
+            {
+                state = IGNORE;
+            }
+
+            if (str[index] == endBracket)
             {
                 state = SCANNING;
-
             }
-        }
 
-        if (str[index] == startBracket)
-        {
-            state = IGNORE;
-        }
+            if (str[index] == ',' && state != IGNORE) {
+                previousComma = index;
+            }
 
-        if (str[index] == endBracket)
-        {
-            state = SCANNING;
+            // The last
+            if (index == str.length() - 1)
+            {
+                QString field = str.mid(start, index);
+                list.append(field.trimmed());
+            }
+            index++;
         }
-
-        if (str[index] == ',' && state != IGNORE) {
-            previousComma = index;
-        }
-
-        // The last
-        if (index == str.length() - 1)
-        {
-            QString field = str.mid(start, index);
-            list.append(field.trimmed());
-        }
-        index++;
-    }
         return list;
     }
 
