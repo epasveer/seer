4d99508adb1c4cba0e2a6b3e04891fede6d6f1e4
QuangNguyenMinh123 <100016403+QuangNguyenMinh123@users.noreply.github.com>
2025-11-12 10:46:02 +0700

    update test

--- tests/recursive_test/include/advanced.h
+++ tests/recursive_test/include/advanced.h
@@ -1,5 +1,32 @@
 #ifndef ADVANCED_H
 #define ADVANCED_H
+#include <iostream>
+#include <iostream>
+#include <chrono>
+#include <unistd.h>
+#include <string>
+#include <vector>
+
+struct childStruct
+{
+    int age;
+    std::string name;
+};
+
+struct parentStruct
+{
+    int age;
+    std::string name;
+    childStruct childArr[100];
+};
+
+struct familyStruct
+{
+    parentStruct dad;
+    int numberOfNumber;
+    std::string familyName;
+    parentStruct mom;
+};
 
 int factorial(int n);
 int fibonacci(int n);
