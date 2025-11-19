df35f46c67066bbabbd9b813f70989a9e1bf41d0
QuangNguyenMinh123 <100016403+QuangNguyenMinh123@users.noreply.github.com>
2025-11-19 09:34:58 +0700

    Revert "Revert "update test case""
    This reverts commit 9e303bd266ef496a8718a999e212a870deb467c3.

--- tests/recursive_test/src/advanced.cpp
+++ tests/recursive_test/src/advanced.cpp
@@ -46,15 +46,47 @@
  * 
  */
 int factorial(int n) {
-    struct structTetst testStruct = {-2, "test", {-1, "childTest"}};
-    testStruct.id ++;
-    testStruct.id ++;
-    testStruct.id ++;
+    struct familyStruct family = {
+        // DAD
+        {
+            50,
+            "DAD_NAME",
+            {
+                {1,
+                "1st_child"},
+                {2,
+                "2nd_child"},
+                {3,
+                "3rd_child"},
+            }
+        },
+        4, "FAMILY", 
+        
+        // MOM
+        {
+            45,
+            "MOM_NAME",
+            {
+                {'a',
+                "a_child"},
+                {'b',
+                "b_child"},
+                {'c',
+                "c_child"},
+            }
+
+        }
+    };
+    family.numberOfNumber ++;
+    family.numberOfNumber ++;
+    family.numberOfNumber ++;
     int abc = 0;
     abc ++;
     abc *= 2;
-    testStruct.child.childId ++;
-    testStruct.child.childString = "childTest " + std::to_string(abc);
+    family.dad.childArr[0].age ++;
+    family.dad.childArr[1].name = "childTest " + std::to_string(abc);
+    family.mom.childArr[1].age ++;
+    family.mom.childArr[0].name = "childTest " + std::to_string(abc) + std::to_string(abc);
     if (n <= 1) return 1;
     return n * factorial(n - 1);
 }
