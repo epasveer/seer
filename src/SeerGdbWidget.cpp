d3e9cc49ee25dd865e4a17401a15b3ef2360d2f4
QuangNguyenMinh123 <100016403+QuangNguyenMinh123@users.noreply.github.com>
2025-11-30 12:39:41 +0700

    Add Ctrl+Shift+T shortcut

--- src/SeerGdbWidget.cpp
+++ src/SeerGdbWidget.cpp
@@ -1688,6 +1688,9 @@ void SeerGdbWidget::handleGdbTerminateExecutable (bool confirm) {
 
             // Kill the gdb.
             killGdb();
+            
+            // Clear the closed files stack.
+            editorManagerWidget->clearClosedFilesStack();
 
             // Print a message.
             addMessage("Program terminated.", QMessageBox::Warning);
