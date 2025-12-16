8764eb1137d82945e961494d78f0ad14edcaf5dd
Ernie Pasveer <epasveer@att.net>
2025-12-16 13:41:24 -0600

    Rename icon from seergdb to seer.

--- src/resources/flathub/BUILD.sh
+++ src/resources/flathub/BUILD.sh
@@ -4,8 +4,8 @@
 
 flatpak uninstall -y io.github.epasveer.seer
 \rm -rf build-dir/ repo/ .flatpak-builder/ seer.flatpak
-flatpak run org.flatpak.Builder --force-clean build-dir ../src/resources/flathub/io.github.epasveer.seer.yml
-flatpak run org.flatpak.Builder --force-clean --repo=repo build-dir ../src/resources/flathub/io.github.epasveer.seer.yml
+flatpak run org.flatpak.Builder --force-clean build-dir io.github.epasveer.seer.yml
+flatpak run org.flatpak.Builder --force-clean --repo=repo build-dir io.github.epasveer.seer.yml
 flatpak build-bundle repo seer.flatpak io.github.epasveer.seer master
 flatpak install -y --bundle --user seer.flatpak
 
