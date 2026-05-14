2cbb9c1a1e67d206257b82f1c37ccdbd288f98f8
QuangNguyenMinh123 <100016403+QuangNguyenMinh123@users.noreply.github.com>
2026-05-14 22:49:44 +0700

    Add actionDebugOnInit and handler

--- src/SeerGdbWidget.cpp
+++ src/SeerGdbWidget.cpp
@@ -4242,3 +4242,10 @@ void SeerGdbWidget::handleGdbMultiarchOpenOCDExecutable ()
 
     qCDebug(LC) << "Finishing 'gdb-multiarch connect'.";
 }
+
+/***********************************************************************************************************************
+ * Start of handle Debug On Init
+ **********************************************************************************************************************/
+void SeerGdbWidget::handleDebugOnInit () {
+
+}
